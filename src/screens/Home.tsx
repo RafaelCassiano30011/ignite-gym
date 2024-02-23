cb143b40dca57dba1767ba50457bc475e7d03c12
Brown <rafael30011@gmail.com>
2024-02-23 20:24:15 -0300

    feat: groups

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,51 @@
+import { Group } from "@components/Group";
 import HomeHeader from "@components/HomeHeader";
-import { VStack } from "native-base";
+import { HStack, VStack, FlatList } from "native-base";
+import { useState } from "react";
+
+const groupsMoc = [
+  "perna",
+  "costa",
+  "ombro",
+  "peito",
+  "biceps",
+  "triceps",
+  "abdomen",
+  "antebraco",
+  "panturrilha",
+  "gluteo",
+];
 
 export function Home() {
+  const [groups, setGroups] = useState(groupsMoc); // [1
+  const [groupSelected, setGroupSelected] = useState("perna");
+
+  const handleGroupSelected = (group: string) => {
+    setGroupSelected(group);
+  };
+
+  console.log(groups);
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <HStack>
+        <FlatList
+          data={groups}
+          keyExtractor={(group) => group}
+          horizontal
+          renderItem={({ item: group }) => (
+            <Group onPress={() => handleGroupSelected(group)} name={group} isActive={groupSelected === group} />
+          )}
+          showsHorizontalScrollIndicator={false}
+          _contentContainerStyle={{
+            px: 8,
+            my: 10,
+            maxHeight: 10,
+          }}
+        />
+      </HStack>
     </VStack>
   );
 }
