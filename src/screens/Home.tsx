dfece11ceff8a7fc5b0f4414abfc01daa8b3558f
Brown <rafael30011@gmail.com>
2024-02-26 21:28:57 -0300

    feat: add list exerciseCard

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,7 @@
+import { ExerciseCard } from "@components/ExerciseCard";
 import { Group } from "@components/Group";
 import HomeHeader from "@components/HomeHeader";
-import { HStack, VStack, FlatList } from "native-base";
+import { HStack, VStack, FlatList, Heading, Text } from "native-base";
 import { useState } from "react";
 
 const groupsMoc = [
@@ -16,15 +17,16 @@ const groupsMoc = [
   "gluteo",
 ];
 
+const exercisesMoc = ["Puxada Alta", "Remada"];
+
 export function Home() {
   const [groups, setGroups] = useState(groupsMoc); // [1
+  const [exercises, setExercises] = useState(exercisesMoc); // [1
   const [groupSelected, setGroupSelected] = useState("perna");
 
   const handleGroupSelected = (group: string) => {
     setGroupSelected(group);
   };
-
-  console.log(groups);
 
   return (
     <VStack flex={1}>
@@ -46,6 +48,28 @@ export function Home() {
           }}
         />
       </HStack>
+
+      <VStack flex={1} px="8">
+        <HStack justifyContent={"space-between"}>
+          <Heading color={"gray.200"} fontSize={"md"}>
+            Exercicios
+          </Heading>
+
+          <Text color={"gray.200"} fontSize={"sm"}>
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <ExerciseCard />}
+          showsHorizontalScrollIndicator={false}
+          _contentContainerStyle={{
+            paddingBottom: 20,
+          }}
+        />
+      </VStack>
     </VStack>
   );
 }
